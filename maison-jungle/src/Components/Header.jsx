064d2f7f0b2fab50997d245b951f1de07fc19645
boubouuubinks <neqoo.mah@gmail.com>
2023-11-09 11:47:21 +0100

    début du component header

--- maison-jungle/src/Components/Header.jsx
+++ maison-jungle/src/Components/Header.jsx
@@ -2,10 +2,39 @@ import '../styles/Header.scss'
 
 export function Header () {
     const title = "La maison jungle"
+    const navItems = [
+        {
+            label: 'Accueil',
+            path: '/home'
+        },
+        {
+            label: 'Nos plantes',
+            path: '/products'
+        },
+        {
+            label: 'Notre marque',
+            path: '/brand'
+        },
+        {
+            label: 'Contactez-nous',
+            path: '/contact'
+        },
+    ]
     return (
         <div className="container">
-            <img src="../assets/leaf.png"/>
-            <h1>{title}</h1>
+            <div className="wrapper-logo">
+                <img src="../assets/leaf.png"/>
+                <h1>{title}</h1>
+            </div>
+            <div className="wrapper-navigation">
+                <ul>
+                    {
+                        navItems.map((item, index) => (
+                            <li key={index}>{item.label}</li>
+                        ))
+                    }
+                </ul>
+            </div>
         </div>
     )
 }
